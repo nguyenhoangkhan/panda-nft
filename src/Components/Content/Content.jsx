e5e6beec20f83be2cf393b7179010f27d5f74bd0
Khan <nguyenhoangkhan210@gmail.com>
2022-07-16 15:19:04 +0700

    first commit

---
+++ src/Components/Content/Content.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Content = (props) => {
+  return <div>Content</div>;
+};
+
+Content.propTypes = {};
+
+export default Content;
---
+++ src/Components/Grid/Grid.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+const Grid = (props) => {
+  const col = props.col ? props.col : "";
+  const mdCol = props.mdCol ? props.mdCol : "";
+  const smCol = props.smCol ? props.smCol : "";
+  return (
+    <div classNames={`col l-${col} m-${mdCol} c-${smCol}`}>
+      {props.children}
+    </div>
+  );
+};
+Grid.propTypes = {
+  col: PropTypes.number.isRequired,
+  mdCol: PropTypes.number,
+  smCol: PropTypes.number,
+};
+export default Grid;
